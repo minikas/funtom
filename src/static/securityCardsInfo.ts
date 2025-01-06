0a8b9917eb3e76a5f67586bb031802af672a2813
Kas <kas_pintxuki@outlook.com>
2025-01-06 15:09:51 -0300

    Revert "fix: comment video/webm format type"
    This reverts commit 35714979de83bdbb6fb7bf58f34613be1477577c.

--- src/static/securityCardsInfo.ts
+++ src/static/securityCardsInfo.ts
@@ -6,10 +6,10 @@ export const securityCardsInfo = [
         src: "https://cdn.sanity.io/files/3nm6d03a/production/56c73616b5ca840696e649a83602eff7ceeef5a5.mp4#t=0.1",
         type: "video/mp4; codecs='hvc1'",
       },
-      // {
-      //   src: "https://cdn.sanity.io/files/3nm6d03a/production/616871cf48d527d65afcf4d149ba6cd98bb33cd0.webm#t=0.1",
-      //   type: "video/webm",
-      // },
+      {
+        src: "https://cdn.sanity.io/files/3nm6d03a/production/616871cf48d527d65afcf4d149ba6cd98bb33cd0.webm#t=0.1",
+        type: "video/webm",
+      },
     ],
     text: "Self-custodial means you control your funds. We never have access..",
   },
@@ -20,10 +20,10 @@ export const securityCardsInfo = [
         src: "https://cdn.sanity.io/files/3nm6d03a/production/9b216878ac8d0cbafc20a8535cdf98297225256d.mp4#t=0.1",
         type: "video/mp4; codecs='hvc1'",
       },
-      // {
-      //   src: "https://cdn.sanity.io/files/3nm6d03a/production/33b3545100c2b7d25e2a5eb51ee45729bf07f551.webm#t=0.1",
-      //   type: "video/webm",
-      // },
+      {
+        src: "https://cdn.sanity.io/files/3nm6d03a/production/33b3545100c2b7d25e2a5eb51ee45729bf07f551.webm#t=0.1",
+        type: "video/webm",
+      },
     ],
     text: "Private by design. No name, email, or phone number required.",
   },
@@ -34,10 +34,10 @@ export const securityCardsInfo = [
         src: "https://cdn.sanity.io/files/3nm6d03a/production/380ecb84fee71f4b8f5f22a29dc98fa73b917831.mp4#t=0.1",
         type: "video/mp4; codecs='hvc1'",
       },
-      // {
-      //   src: "https://cdn.sanity.io/files/3nm6d03a/production/ef9e9258b2cb966a1748ef70d71706c171856e14.webm#t=0.1",
-      //   type: "video/webm",
-      // },
+      {
+        src: "https://cdn.sanity.io/files/3nm6d03a/production/ef9e9258b2cb966a1748ef70d71706c171856e14.webm#t=0.1",
+        type: "video/webm",
+      },
     ],
     text: "Scam detection flags malicious transactions instantly.",
   },
@@ -48,10 +48,10 @@ export const securityCardsInfo = [
         src: "https://cdn.sanity.io/files/3nm6d03a/production/74f58521ae1d7925d27bcbdf45395a2274d11d7c.mp4#t=0.1",
         type: "video/mp4; codecs='hvc1'",
       },
-      // {
-      //   src: "https://cdn.sanity.io/files/3nm6d03a/production/0f1866916249a4bf118507a135c7ba233f48e91a.webm#t=0.1",
-      //   type: "video/webm",
-      // },
+      {
+        src: "https://cdn.sanity.io/files/3nm6d03a/production/0f1866916249a4bf118507a135c7ba233f48e91a.webm#t=0.1",
+        type: "video/webm",
+      },
     ],
     text: "Connect your Ledger to keep your crypto even safer.",
   },
@@ -62,10 +62,10 @@ export const securityCardsInfo = [
         src: "https://cdn.sanity.io/files/3nm6d03a/production/47d4408a43895b2e1ab0a401b1bda37bae723a94.mp4#t=0.1",
         type: "video/mp4; codecs='hvc1'",
       },
-      // {
-      //   src: "https://cdn.sanity.io/files/3nm6d03a/production/615b22df6c77187d858a199f934d991df99ea428.webm#t=0.1",
-      //   type: "video/webm",
-      // },
+      {
+        src: "https://cdn.sanity.io/files/3nm6d03a/production/615b22df6c77187d858a199f934d991df99ea428.webm#t=0.1",
+        type: "video/webm",
+      },
     ],
     text: "Our global Support team is here for you 24/7.",
   },
